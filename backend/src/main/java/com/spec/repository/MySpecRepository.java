39c33e7ef61721cc472e00b2be25852456325839
choys <charm4860as@gmail.com>
2025-05-27 20:00:18 +0900

    로그인 회원가입 쪽 구현 마무리리

--- backend/src/main/java/com/spec/repository/MySpecRepository.java
+++ backend/src/main/java/com/spec/repository/MySpecRepository.java
@@ -1,8 +1,8 @@
 package com.spec.repository;
 
+import com.spec.domain.MySpec;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
-
 
 public interface MySpecRepository extends MongoRepository<MySpec, String> {
     Optional<MySpec> findByUserId(String userId);
